e906633a5d3dc707a2ca254bfd795ca13ad18598
Omar-essayed-1122 <omaressayed02@gmail.com>
2025-02-18 23:24:48 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,26 +7,25 @@ import './assets/css/tailwind.css';
 
 // استيراد Font Awesome
 import '@fortawesome/fontawesome-free/css/all.css';
+
 // استيراد animate.css
 import 'animate.css';
 
-// تحميل BootstrapVue 3 بشكل كسول
+// تحميل BootstrapVue 3 قبل أي شيء
+import BootstrapVue3 from 'bootstrap-vue-3';
+
+import VueGtag from 'vue-gtag';
+
 const app = createApp(App);
 
-
+// تثبيت المكتبات قبل التركيب
+app.use(BootstrapVue3);
 app.use(router);
 
-import BootstrapVue3 from 'bootstrap-vue-3';
-app.use(BootstrapVue3);
+// تحميل Google Analytics مباشرةً
+app.use(VueGtag, {
+  config: { id: 'G-ZRWXVV69P4' }
+}, router);
 
-
-  // تحميل Google Analytics بشكل كسول
-  import('vue-gtag').then((VueGtag) => {
-    app.use(VueGtag, {
-      config: { id: 'G-ZRWXVV69P4' }
-    }, router);
-  });
-
-
-// تركيب التطبيق
+// تركيب التطبيق بعد التأكد من تحميل كل شيء
 app.mount('#app');
